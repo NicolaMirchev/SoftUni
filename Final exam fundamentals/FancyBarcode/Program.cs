fbe6171c20602c3c74be4e51523df7b33a215d1a
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-08-10 11:34:52 +0300

    Excersice with RegEx

---
+++ Final exam fundamentals/FancyBarcode/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace FancyBarcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            int barcodesNum = int.Parse(Console.ReadLine());
+            string pattern = @"\@\#+[A-Z](?:[a-z]|[\d]|[A-Z])(?:[a-z]|[\d]|[A-Z])(?:[a-z]|[\d]|[A-Z])(?:[a-z]|[\d]|[A-Z])+[A-Z]\@\#+";
+
+            var regex = new Regex(pattern);
+
+            for (int i = 0; i < barcodesNum; i++)
+            {
+                string text = Console.ReadLine();
+
+                if (regex.IsMatch(text))
+                {
+                    string group = null;
+
+                    for (int j = 0; j < text.Length; j++)
+                    {
+                        if (char.IsDigit(text[j]))
+                        {
+                            group += text[j];
+                        }
+                    }
+
+                    if (group == null)
+                    {
+                        Console.WriteLine("Product group: 00");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Product group: {group}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid barcode");
+                }
+            }
+
+        }
+    }
+}
