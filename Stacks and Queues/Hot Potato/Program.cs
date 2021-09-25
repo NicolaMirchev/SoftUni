7bdbcb9928c91ad5804421b1bcfa0f2876ea757e
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-09-25 11:05:07 +0300

    Exersice

---
+++ Stacks and Queues/Hot Potato/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+
+namespace Hot_Potato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] people = Console.ReadLine().Split();
+            int n = int.Parse(Console.ReadLine());
+
+            Queue queue = new Queue(people);
+
+            while (queue.Count > 1)
+            {
+                for (int i = 1; i < n; i++)
+                {
+                    queue.Enqueue(queue.Dequeue());
+                }
+
+                var Name = queue.Dequeue();
+                Console.WriteLine($"Removed {Name}");
+            }
+
+            Console.WriteLine($"Last is {queue.Dequeue()}");
+        }
+        
+    }
+}
