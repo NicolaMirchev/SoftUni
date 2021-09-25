9029065b7f94b90cd851572d4cf9a61ab07c1a0d
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-09-25 11:09:23 +0300

    Excersise

---
+++ Stacks and Queues/PrintEven/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PrintEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            List<int> list = new List<int>();
+
+            foreach (var item in numbers)
+            {
+                if (item % 2 == 0)
+                {
+                    list.Add(item);
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", list));
+        
+        }
+    }
+}
---
+++ Stacks and Queues/Simple Calculator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+
+namespace Simple_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Stack stack = new Stack();
+
+            for (int i = 0; i < input.Length; i++)            
+            {
+                if (input[i] == '(')
+                {
+                    stack.Push(i);
+                }
+                else if (input[i] == ')')
+                {
+                    int endingIndex = i;
+                    int startingIndex = (int)stack.Pop();
+                    string result = input.Substring(startingIndex, endingIndex - startingIndex + 1);
+
+                    Console.WriteLine(result);
+                }
+
+
+            }
+        }
+    }
+}
