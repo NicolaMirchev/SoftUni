e424f8281242503e1da84ba78801501842c6c508
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-09-25 11:16:25 +0300

    Exercise

---
+++ Stacks and Queues/TrafficJam/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+
+namespace TrafficJam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int capacity = int.Parse(Console.ReadLine());
+
+            Queue queue = new Queue();
+            int counter = 0;
+            string command = Console.ReadLine();
+
+            while (command.ToLower() != "end")
+            {
+                if (command == "green")
+                {
+                    for (int i = 0; i < capacity; i++)
+                    {
+                        if (queue.Count >= 1)
+                        {
+
+
+                            Console.WriteLine($"{queue.Dequeue()} passed!");
+                            counter++;
+                        }
+                    }
+                }
+                else
+                {
+                    queue.Enqueue(command);
+                    
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"{counter} cars passed the crossroads.");
+        }
+    }
+}
