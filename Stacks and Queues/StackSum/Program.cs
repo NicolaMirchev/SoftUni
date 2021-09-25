828c39e731ea3b727f102694abe175dd6f111c3f
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-09-25 11:15:28 +0300

    Exercise

---
+++ Stacks and Queues/StackSum/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace StackSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            string command = Console.ReadLine();
+
+            Stack stack = new Stack(numbers);
+
+            while (command.ToLower() != "end")
+            {
+                string[] data = command.Split();
+
+                if (data[0].ToLower() == "add")
+                {
+
+                    stack.Push(int.Parse(data[1]));
+                    stack.Push(int.Parse(data[2]));
+                }
+                else if (data[0].ToLower() == "remove")
+                {
+                    if (stack.Count >= int.Parse(data[1]))
+                    {
+                        for (int i = 0; i < int.Parse(data[1]); i++)
+                        {
+                            stack.Pop();
+                        }
+                    }
+                }
+                command = Console.ReadLine();
+            }
+            int sum = 0;
+            foreach (var item in stack)
+            {
+                sum += (int)item;
+            }
+
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
---
+++ Stacks and Queues/Supermarket/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+
+namespace Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Queue queue = new Queue();
+
+            while (input != "End")
+            {
+                if (input == "Paid")
+                {
+                    while (queue.Count > 0)
+                    {
+                        Console.WriteLine(queue.Dequeue());
+                    }
+                }
+                else
+                {
+                    queue.Enqueue(input);
+                }
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"{queue.Count} people remaining.");
+        }
+    }
+}
