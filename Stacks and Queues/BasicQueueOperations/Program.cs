047feca909ea997bb33de9f13af7c7cd3fb9c625
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-09-26 20:21:08 +0300

    Add files via upload

---
+++ Stacks and Queues/BasicQueueOperations/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace BasicQueueOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] data = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int queueCap = data[0];
+            int elementToDequeue = data[1];
+            int elementToSeek = data[2];
+
+            Queue queue = new Queue(Console.ReadLine()
+                     .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                     .Select(int.Parse)
+                     .ToArray());
+
+            for (int i = 0; i < elementToDequeue; i++)
+            {
+                queue.Dequeue();
+            }
+
+            if (queue.Count > 0)
+            {
+                if (queue.Contains(elementToSeek))
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    int smallestNum = int.MaxValue;
+                    while (queue.Count > 0)
+                    {
+                        int currentNum = (int)queue.Dequeue();
+                        if (currentNum < smallestNum)
+                        {
+                            smallestNum = currentNum;
+                        }
+                    }
+                    Console.WriteLine(smallestNum);
+                }
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
+        }
+    }
+}
