e347934817befbf8fc7127585fce47f8cb1da68e
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-09-26 20:17:07 +0300

    Exersice

---
+++ Multidimentional arrays/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Multidimentional_arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] data = Console.ReadLine().Split(", ");
+
+            int rows = int.Parse(data[0]);
+            int cols = int.Parse(data[1]);
+
+            int[,] matrix = new int[rows, cols];
+
+            for (int rowss = 0; rowss < matrix.GetLength(0); rowss++)
+            {
+                string[] lineData = Console.ReadLine().Split(", ");
+
+
+                for (int colls = 0; colls < matrix.GetLength(1); colls++)
+                {
+                    matrix[rowss, colls] = int.Parse(lineData[colls]);
+                }
+
+            }
+            Console.WriteLine(rows);
+            Console.WriteLine(cols);
+            int sum = 0;
+
+            foreach (var item in matrix)
+            {
+                sum += item;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
