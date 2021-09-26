6e9f02c04b7ca040a40d20a0c1245756ab4f0321
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-09-26 20:20:14 +0300

    Exercise

---
+++ Stacks and Queues/Bourique/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bourique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>(Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray());
+
+            int rackCapacity = int.Parse(Console.ReadLine());
+
+            int sumOfCurrentRack = 0;
+            int numOfRacks = 1;
+            while (stack.Count > 0)
+            {
+                if (stack.Peek() + sumOfCurrentRack <= rackCapacity)
+                {
+                    sumOfCurrentRack += stack.Pop();
+                }
+                else
+                {
+                    numOfRacks++;
+                    sumOfCurrentRack = 0;
+                    sumOfCurrentRack += stack.Pop();
+                }
+
+            }
+            Console.WriteLine(numOfRacks);
+        }
+    }
+}
