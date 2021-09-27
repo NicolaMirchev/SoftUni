21aba0d83f34e26e3ba1859dead54b32f6049148
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-09-27 11:06:06 +0300

    Exersice

---
+++ Multidimentional arrays/PrimaryDiagonal/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PrimaryDiagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+
+            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                string[] data = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                for (int colls = 0; colls < matrix.GetLength(1); colls++)
+                {
+
+                    matrix[rows, colls] = int.Parse(data[colls]);
+                }
+            }
+            int sum = 0;
+            for (int number = 0; number < matrix.GetLength(0); number++)
+            {
+
+                sum += matrix[number, number];
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
