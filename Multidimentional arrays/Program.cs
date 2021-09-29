b2abeb1c01a1fc01fef06d12909b06fbd6bd68ce
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-09-29 13:56:00 +0300

    Excersice

--- Multidimentional arrays/Program.cs
+++ Multidimentional arrays/Program.cs
@@ -1,38 +1,73 @@
 ﻿using System;
 
-namespace Multidimentional_arrays
+namespace MatrixSuffering
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string[] data = Console.ReadLine().Split(", ");
+            string[] matrixSize = Console.ReadLine().Split(' ');
+            int rolsSize = int.Parse(matrixSize[0]);
+            int colsSize = int.Parse(matrixSize[1]);
 
-            int rows = int.Parse(data[0]);
-            int cols = int.Parse(data[1]);
+            string[,] matrix = new string[rolsSize, colsSize];
 
-            int[,] matrix = new int[rows, cols];
-
-            for (int rowss = 0; rowss < matrix.GetLength(0); rowss++)
+            for (int row = 0; row < rolsSize; row++)
             {
-                string[] lineData = Console.ReadLine().Split(", ");
+                string[] input = Console.ReadLine().Split(' ');
 
-
-                for (int colls = 0; colls < matrix.GetLength(1); colls++)
+                for (int col = 0; col < colsSize; col++)
                 {
-                    matrix[rowss, colls] = int.Parse(lineData[colls]);
+                    matrix[row, col] = input[col];
+                }
+            }
+
+            string command = Console.ReadLine();
+
+            while (command.ToLower() != "end")
+            {
+                string[] data = command.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                int firstRow = int.Parse(data[1]);
+                int firstCol = int.Parse(data[2]);
+                int secondRow = int.Parse(data[3]);
+                int secondCol = int.Parse(data[4]);
+
+                if (data[0] != "swap" || data.Length != 5)
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+                else if (firstRow >= matrix.GetLength(0) || secondRow >= matrix.GetLength(1))
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+                else if (firstCol >= matrix.GetLength(1) || secondCol >= matrix.GetLength(1))
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+                else if (firstCol < 0 || firstRow < 0 || secondCol < 0 || secondRow < 0)
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+                else
+                {
+                    string firstString = matrix[firstRow, firstCol];
+                    matrix[firstRow, firstCol] = matrix[secondRow, secondCol];
+                    matrix[secondRow, secondCol] = firstString;
+
+                    for (int row = 0; row < matrix.GetLength(0); row++)
+                    {
+                        for (int col = 0; col < matrix.GetLength(1); col++)
+                        {
+                            Console.Write(matrix[row, col] + " ");
+                        }
+                        Console.WriteLine();
+                    }
                 }
 
-            }
-            Console.WriteLine(rows);
-            Console.WriteLine(cols);
-            int sum = 0;
 
-            foreach (var item in matrix)
-            {
-                sum += item;
+
+                command = Console.ReadLine();
             }
-            Console.WriteLine(sum);
         }
     }
 }
