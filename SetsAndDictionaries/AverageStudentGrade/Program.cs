8eacab163f0fbc10cfe089f674677b35280000c0
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-09-30 18:39:33 +0300

    Exersice

---
+++ SetsAndDictionaries/AverageStudentGrade/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AverageStudentGrade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfStudents = int.Parse(Console.ReadLine());
+            var studentGrades = new Dictionary<string, List<double>>();
+
+            for (int i = 0; i < numberOfStudents; i++)
+            {
+                string[] data = Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries);
+                string name = data[0];
+                double grade = double.Parse(data[1]);
+
+                if (studentGrades.ContainsKey(name))
+                {
+                    studentGrades[name].Add(grade);
+                }
+                else
+                {
+                    studentGrades.Add(name, new List<double>() { grade });
+                }
+            }
+
+            foreach (var student in studentGrades)
+            {
+                Console.Write($"{student.Key} -> ");
+                foreach (var grade in student.Value)
+                {
+                    Console.Write($"{grade:f2} ");
+                }
+                Console.WriteLine($"(avg: {student.Value.Average():f2})");
+            }
+                
+        }
+    }
+}
