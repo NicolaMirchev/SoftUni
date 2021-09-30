613218d9b51e3f883d00fef29796360bbd4be9b4
Nicola.mirchev@gmail.com <nicola.mirchev@gmail.com>
2021-09-30 16:25:12 +0300

    ComittingInVS

---
+++ SetsAndDictionaries/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SetsAndDictionaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double[] inputData = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                                                   .Select(double.Parse).ToArray();
+            Dictionary<double, int> dict = new Dictionary<double, int>();
+            
+            foreach (var num in inputData)
+            {
+                if (dict.ContainsKey(num))
+                {
+                    dict[num]++;
+                }
+                else
+                {
+                    dict.Add(num, 1);
+                }
+
+            }
+
+
+            foreach (var num in dict)
+            {
+                
+                Console.WriteLine($"{num.Key} - {num.Value} times");
+            }
+        }
+    }
+}
---
+++ SnakeMoves/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace SnakeMoves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] matrixSize = Console.ReadLine().Split(' ');
+            int rolsSize = int.Parse(matrixSize[0]);
+            int colsSize = int.Parse(matrixSize[1]);
+
+            char[,] matrix = new char[rolsSize, colsSize];
+
+            string word = Console.ReadLine();
+            int counter = 0;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                
+
+                if (row % 2 == 0)
+                {
+                    for (int col = 0; col < matrix.GetLength(1); col++)
+                    {
+                      
+                        matrix[row, col] = (char)word[counter];
+                        if (counter == word.Length - 1)
+                        {
+                            counter = 0;
+                        }
+                        else
+                        {
+                            counter++;
+                        }
+                        
+                    }
+
+                }
+                else
+                {
+                    for (int col = matrix.GetLength(1) - 1; col >= 0; col--)
+                    {
+                        matrix[row, col] = (char)word[counter];
+                        if (counter == word.Length - 1)
+                        {
+                            counter = 0;
+                        }
+                        else
+                        {
+                            counter++;
+                        }
+                    }
+                } 
+                
+                
+            }
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+                Console.WriteLine( );
+            }
+        }
+    }
+}
