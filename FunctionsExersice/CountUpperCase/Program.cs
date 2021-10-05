d4805f1a7f493185ca977cfa104f97830b790038
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-10-05 17:02:43 +0300

    Exersice

---
+++ FunctionsExersice/CountUpperCase/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace CountUpperCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+          
+
+            Console.WriteLine(string.Join(Environment.NewLine, input.Where(word => char.IsUpper(word[0])).ToArray())); 
+            
+
+            
+        }
+    }
+}
