6380f0d8f59e5d2831756afcb704b8bce68f08f0
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-10-05 16:50:26 +0300

    Add files via upload

---
+++ FunctionsExersice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FunctionsExersice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
