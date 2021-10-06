04a37bf722cebcc542f250c86bc5d4fba01de13b
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-10-06 20:22:01 +0300

    Exercise

---
+++ FunctionsExersice/TriFunctions/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace TriFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int N = int.Parse(Console.ReadLine());
+
+            string[] names = Console.ReadLine().Split();
+
+
+            Func<string, int, bool> firstFunc = (word, number) => word.Sum(x => x) >= number;
+
+            Func<string[],Func<string, int, bool>,string> secondFunc = (list, secondFunc) => list.FirstOrDefault((x) => secondFunc(x, N));
+
+            Console.WriteLine(secondFunc(names, firstFunc));
+        }
+    }
+}
