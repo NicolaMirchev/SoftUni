c2990a0d83104cf446e533275ea70c801cb81ded
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-10-06 20:24:06 +0300

    Exercise

---
+++ FunctionsExersice/CustomMinFunction/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CustomMinFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Console.WriteLine(numbers.Min());
+        }
+    }
+}
