8dc446b23cfedf392304cbb94eb8770d0486a3bb
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-10-07 17:32:58 +0300

    Exercise

---
+++ FunctionsExersice/EvenOrOdd/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace EvenOrOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] range = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            string command = Console.ReadLine();
+
+            Predicate<int> isEven = x => x % 2 == 0;
+
+            for (int i = range[0]; i <= range[1]; i++)
+            {
+                if (command == "even")
+                {
+                    if (isEven(i))
+                    {
+                        Console.Write($"{i} ");
+                    }
+                }
+                else if (command == "odd")
+                {
+                    if (!isEven(i))
+                    {
+                        Console.WriteLine($"{i} ");
+                    }
+
+                }
+                
+                
+
+                
+
+            }
+        }
+    }
+}
