0a1c9a1ba407eb162993553ccc7e6ff92db6abe2
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-10-07 17:29:10 +0300

    Exersice

---
+++ FunctionsExersice/PartyPeolpe/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace PartyPeolpe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] guests = Console.ReadLine().Split(' ');
+
+            string command = Console.ReadLine();
+
+            while (command.ToLower() != "Party!")
+            {
+                string[] data = command.Split(' ') ;
+                if (data[0].ToLower() == "remove")
+                {
+                    if (true)
+                    {
+
+                    }
+
+                }
+                else if (data[0].ToLower() == "double")
+                {
+
+                }
+
+
+
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
