b3e2888b9dacaf57413be9d504408e0ffaad14ca
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-10-09 22:33:42 +0300

    Exercise

---
+++ FunctionsExersice/DivisibleByN/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DivisibleByN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] data = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int number = int.Parse(Console.ReadLine());
+
+            Predicate<int> isTrue = x => x % number != 0;
+
+            var result = new List<int>();
+
+            foreach (var item in data)
+            {
+                if (item % number != 0)
+                {
+                    result.Add(item);
+                }
+            }
+
+            result.Reverse();
+            Console.WriteLine(string.Join(' ', result));
+            
+
+            
+        }
+    }
+}
