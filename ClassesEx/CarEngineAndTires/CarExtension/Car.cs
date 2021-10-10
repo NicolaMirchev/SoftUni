346743d36cb5adb26869c9e6057127a8caeb2dcb
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-10-10 14:16:31 +0300

    PreFinal class info

---
+++ ClassesEx/CarEngineAndTires/CarExtension/Car.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    class Car
+    {
+
+        public Car()
+        { 
+        }
+        public Car(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption)  : this(make, model, year)
+        {
+
+            FuelQuantity = fuelQuantity;
+            FuelConsumption = fuelConsumption;
+        
+        }
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public double FuelQuantity { get; set; }
+        public double FuelConsumption { get; set; }
+
+        public void Drive(double distance)
+        {
+            if (FuelQuantity - distance * (FuelConsumption / 100) >= 0)
+            {
+                FuelQuantity -= distance * (FuelConsumption / 100);
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            return $"Make: {this.Make}\n" +
+                $"Model {this.Model}\n" +
+                $"Year {this.Year}\n" +
+                $"Fuel: {this.FuelQuantity:f2}";
+        }
+
+    }
+}
---
+++ ClassesEx/CarEngineAndTires/CarExtension/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CarManufacturer;
+ namespace CarManufacturer
+{
+   public class StartUp
+    {
+       public static void Main(string[] args)
+        {
+
+            string make = Console.ReadLine();
+            string model = Console.ReadLine();
+            int year = int.Parse(Console.ReadLine());
+            double fuelQuantity = 111;
+            double fuelConsumption = 11;
+
+            var firstCar = new Car();
+            var secondCar = new Car(make, model, year);
+            var thirdCar = new Car(make, model, year, fuelQuantity, fuelConsumption);
+        }
+    }
+}
