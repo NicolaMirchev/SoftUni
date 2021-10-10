ac6828a308feb3340a740203b71b327f68485789
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-10-10 14:15:12 +0300

    First complex exercise using classes

---
+++ ClassesEx/CarEngineAndTires/Car.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarEngineAndTires
+{
+    class Car
+    {
+        public Car()
+        {
+        }
+        public Car(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption) : this(make, model, year)
+        {
+
+            FuelQuantity = fuelQuantity;
+            FuelConsumption = fuelConsumption;
+
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption, Engine engine, Tires[] tireSet)
+            :this(make,model,year,fuelQuantity,fuelConsumption)
+        {
+            engine = Engine;
+            TireSet = new Tires[4];                               
+            
+        }
+        public Tires[] TireSet { get; set; }
+        public Engine Engine { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public double FuelQuantity { get; set; }
+        public double FuelConsumption { get; set; }
+
+        public void Drive(double distance)
+        {
+            if (FuelQuantity - distance * (FuelConsumption / 100) >= 0)
+            {
+                FuelQuantity -= distance * (FuelConsumption / 100);
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            return $"Make: {this.Make}\n" +
+                $"Model {this.Model}\n" +
+                $"Year {this.Year}\n" +
+                $"Fuel: {this.FuelQuantity:f2}";
+        }
+
+    }
+
+     public class Engine
+    {
+        public Engine(int hp, double cubicCap)
+        {
+            HorsePower = hp;
+            CubicCapacity = cubicCap;
+        }
+
+        public int HorsePower { get; set; }
+
+        public double CubicCapacity { get; set; }
+    }
+
+    public class Tires
+    {
+        public Tires(int year, double pressure)
+        {
+            Year = year;
+            Pressure = pressure;
+        }
+
+        public int Year { get; set; }
+        public double Pressure { get; set; }
+
+    }
+}
+
---
+++ ClassesEx/CarEngineAndTires/StartUp.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CarEngineAndTires;
+
+namespace CarEngineAndTires
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Tires[]> setsOfTires = new List<Tires[]>();
+
+            string command = Console.ReadLine();
+            while (command != "No more tires")
+            {
+                string[] dataAboutTiers = command.Split(' ');
+                var currentTireSet = new Tires[4];
+                int counter = 0;
+
+                for (int i = 0; i < dataAboutTiers.Length; i += 2)
+                {
+                    int year = int.Parse(dataAboutTiers[i]);
+                    double pressure = double.Parse(dataAboutTiers[i + 1]);
+
+                    var tire = new Tires(year, pressure);
+
+                    currentTireSet[counter] = tire;
+                    if (counter >= 4)
+                    {
+                        Console.WriteLine("Error");
+                    }
+                    counter++;
+                }
+
+                setsOfTires.Add(currentTireSet);
+                              
+                command = Console.ReadLine();
+            }
+
+
+            var engines = new List<Engine>();
+
+            command = Console.ReadLine();
+            while (command != "Engines done")
+            {
+                string[] data = command.Split(' ');
+                int HP = int.Parse(data[0]);
+                double cubics = double.Parse(data[1]);
+
+                var currentEngine = new Engine(HP, cubics);
+                engines.Add(currentEngine);
+
+                command = Console.ReadLine();
+            }
+
+            command = Console.ReadLine();
+            List<Car> specialCars = new List<Car>();
+
+            while (command != "Show special")
+            {
+                string[] data = command.Split(' ');
+                string make = data[0];
+                string model = data[1];
+                int year = int.Parse(data[2]);
+                var fuelQuantiry = double.Parse(data[3]);                            
+                var fuelConsumtion = double.Parse(data[4]);
+
+                var engineIndex = int.Parse(data[5]);
+                var tiresIndex = int.Parse(data[6]);
+
+                var currentCar = new Car(make, model, year, fuelQuantiry, fuelConsumtion);
+                currentCar.TireSet = setsOfTires[tiresIndex];
+                currentCar.Engine = engines[engineIndex];
+
+                if (currentCar.Year >= 2017)
+                {
+                    if (currentCar.Engine.HorsePower > 330)
+                    {
+                        if (IsRightPressure(currentCar.TireSet))
+                        {
+                            specialCars.Add(currentCar);
+                        }
+                    }
+
+                }
+
+                command = Console.ReadLine();
+            }
+
+            foreach (var car in specialCars)
+            {
+                car.Drive(20);
+                var stringBuilder = new StringBuilder();
+                stringBuilder.AppendLine($"Make: {car.Make}");
+                stringBuilder.AppendLine($"Model: {car.Model}");
+                stringBuilder.AppendLine($"Year: {car.Year}");
+                stringBuilder.AppendLine($"HorsePowers: {car.Engine.HorsePower}");
+                stringBuilder.AppendLine($"FuelQuantity: {car.FuelQuantity}");
+                Console.WriteLine(stringBuilder);
+            }
+
+            static bool IsRightPressure(Tires[] tireSet)
+            {
+                    double allTiresPressure = 0;
+                    foreach (var tire in tireSet)
+                    {
+                        allTiresPressure += tire.Pressure;
+                    }
+                    if (allTiresPressure > 9 && allTiresPressure < 10)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                   
+            }
+        }
+    }
+}
