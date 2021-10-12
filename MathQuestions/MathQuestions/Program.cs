d11844c3cded5e24a9e1aceabb5ea212ab159f43
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-10-12 14:00:22 +0300

    Math Qustions project
    Custom Console project with random qustions about the Univeristy questions

---
+++ MathQuestions/MathQuestions/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MathQuestions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            var questions = new Dictionary<string, string>();
+
+            var random = new Random();
+
+            string input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                var question = new byte[16];
+
+
+                random.NextBytes(question);
+
+            }
+        }
+    }
+}
