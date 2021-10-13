54bf7aa76293d51b1a3e9ec3873e4cf6bc2e1d11
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-10-13 11:03:30 +0300

    CustomList

---
+++ ClassesEx/CarEngineAndTires/CustomListClass/ListItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomListClass
+{
+    class ListItem
+    {
+        public int Index { get; set; }
+
+        public int Value { get; set; }
+    }
+}
