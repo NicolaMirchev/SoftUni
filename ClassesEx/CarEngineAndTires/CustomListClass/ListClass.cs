98894da3838285f9e2949f1de9da86a72025f01b
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-10-14 09:54:24 +0300

    Custom List

---
+++ ClassesEx/CarEngineAndTires/CustomListClass/ListClass.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomListClass
+{
+    class ListClass
+    {
+        public ListClass()
+        {
+            Array = new int[2];
+            Indexer = -1;
+        }
+        private int[] Array { get; set; }
+
+        public int Indexer { get; private set; }
+
+        public int Count {
+
+            get
+            {
+                return Indexer + 1;
+            }
+
+
+        
+        }
+
+
+        public void Add(int element)
+        {
+            Indexer++;
+            if (Indexer + 1 > Array.Length)
+            {
+                int[] newArray = new int[Array.Length * 2];
+                for (int i = 0; i < Array.Length; i++)
+                {
+                    newArray[i] = Array[i];
+                }
+
+                Array = newArray;
+
+                Array[Indexer] = element;
+            }
+            else
+            {
+                Array[Indexer] = element;
+            }
+
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index < 0 || index > Indexer)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            Array[index] = 0;
+
+            for (int i = index; i < Indexer; i++)
+            {
+                Array[i] = Array[i + 1];
+            }
+            Array[Indexer--] = 0;
+        }
+
+        public bool Contains(int element)
+        {
+            for (int i = 0; i < Indexer; i++)
+            {
+                if (Array[i] == element)
+                {
+                    return true;                    
+                }
+            }
+            return false;
+        }
+
+        public void Swap(int firstIndex, int secondIndex)
+        {
+            if (firstIndex < 0 || firstIndex > Indexer || secondIndex < 0 || secondIndex > Indexer)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            int savedValue = Array[secondIndex];            //Saved value from second index
+            Array[secondIndex] = Array[firstIndex];
+            Array[firstIndex] = savedValue;
+        }
+    }
+
+
+}
---
+++ ClassesEx/CarEngineAndTires/CustomListClass/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CustomListClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new ListClass();
+
+            list.Add(12);
+            list.Add(13);
+            list.Add(14);
+            list.Add(15);
+            list.Add(16);
+
+            list.RemoveAt(4);
+
+            var booll = list.Contains(16);
+            list.Swap(0, list.Indexer);
+            Console.WriteLine(list.Count);
+        }
+    }
+}
