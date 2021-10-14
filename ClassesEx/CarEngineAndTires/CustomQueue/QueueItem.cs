15c4b11622ca162f56629cf054c49b57176f3c34
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-10-14 10:14:45 +0300

    Custom Queue

---
+++ ClassesEx/CarEngineAndTires/CustomQueue/QueueItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomQueue
+{
+    class QueueItem
+    {
+
+        public QueueItem(int value)
+        {
+            Value = value;
+        }
+        public QueueItem PreviousItem { get; set; }
+
+        public QueueItem NextItem { get; set; }
+
+        public int Value { get; set; }
+
+    }
+}
