7f47074152fd6b1039979da8a29b5fc4a8df76e3
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-10-14 22:21:09 +0300

    Custom Queue

---
+++ ClassesEx/CarEngineAndTires/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CustomQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myQueue = new Queue();
+
+            myQueue.Enqueue(12);
+            myQueue.Enqueue(13);
+            myQueue.Enqueue(14);
+
+            Console.WriteLine(myQueue.Dequeue());
+            Console.WriteLine(myQueue.Peek());
+
+            myQueue.Clear();
+
+            myQueue.Enqueue(1);
+            myQueue.Enqueue(2);
+
+            Action<int> action = x => x += x * 2;
+
+            myQueue.Foreach(action);
+
+        }
+        
+    }
+}
---
+++ ClassesEx/CarEngineAndTires/QueueItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomQueue
+{
+    class QueueItem
+    {
+
+        public QueueItem(int value)
+        {
+            Value = value;
+        }
+
+        public QueueItem NextItem { get; set; }
+
+        public int Value { get; set; }
+
+    }
+}
