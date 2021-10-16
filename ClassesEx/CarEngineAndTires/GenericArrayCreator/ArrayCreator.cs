49f2a1b916f5614188d2686e7dd3956934cd8260
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-10-16 13:21:04 +0300

    Static method in generic class

---
+++ ClassesEx/CarEngineAndTires/GenericArrayCreator/ArrayCreator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericArrayCreator
+{
+    class ArrayCreator<T>
+    {
+        public ArrayCreator()
+        {
+
+        }
+        public static T[] Create(int length, T data)
+        {
+            var array = new T[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                array[i] = data;
+            }
+
+            return array;
+        }
+    }
+}
---
+++ ClassesEx/CarEngineAndTires/GenericArrayCreator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericArrayCreator
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            var newArray = ArrayCreator<string>.Create(12, "I Am The Programmer");
+
+            foreach (var item in newArray)
+            {
+                Console.WriteLine(item);
+            }
+
+            var check = new ArrayCreator<int>();
+
+            var checl = ArrayCreator<List<int>>.Create(4, new List<int>() { 12, 15 });
+
+            foreach (var item in checl)
+            {
+                foreach (var Item in item)
+                {
+                    Console.WriteLine(Item);
+                }
+            }
+        }
+    }
+}
