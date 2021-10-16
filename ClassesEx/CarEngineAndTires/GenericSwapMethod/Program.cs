75c5e33b562cdea33f4bb09a093b6c76d52f6bfe
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-10-16 14:51:55 +0300

    Adding some funcionality to a GenericClas

---
+++ ClassesEx/CarEngineAndTires/GenericSwapMethod/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Linq;
+
+namespace GenericBoxOfString
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int dataLength = int.Parse(Console.ReadLine());
+
+            var newClass = new Box<string>();
+
+            for (int i = 0; i < dataLength; i++)
+            {
+                newClass.Add(Console.ReadLine());
+            }
+
+            int[] indexes = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            newClass.Swap(indexes[0], indexes[1]);
+
+
+            Console.WriteLine(newClass);
+
+        }
+    }
+}
