c7a738313ce798a8c0149490ae16e99aa77ae5fc
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-10-16 13:00:20 +0300

    Exercising Generics Types

---
+++ ClassesEx/CarEngineAndTires/BoxOfT/Box.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    class Box<T>
+    {
+        public Box()
+        {
+            IternalStorage = new List<T>();
+        }
+        public int Count { get => IternalStorage.Count; }      
+
+        private List<T> IternalStorage { get; set; }
+
+        public void Add(T item)
+        {
+            IternalStorage.Add(item);
+        }
+
+        public T Remove()
+        {
+            var itemToReturn = IternalStorage[IternalStorage.Count - 1];
+            IternalStorage.RemoveAt(IternalStorage.Count - 1);
+
+            return itemToReturn;
+        }
+
+}
+}
---
+++ ClassesEx/CarEngineAndTires/BoxOfT/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BoxOfT
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var newBox = new Box<int>();
+            var boxOfStrings = new Box<string>();
+
+            newBox.Add(5);
+            newBox.Add(6);
+            newBox.Add(7);
+
+            Console.WriteLine(newBox.Remove());
+
+            Console.WriteLine(newBox.Count);
+        }
+    }
+}
