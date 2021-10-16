7ad4df5f9ba42ed852cd1570f272db123c8a7b72
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-10-16 13:44:29 +0300

    Using Generic Constrains to specify only reference type

---
+++ ClassesEx/CarEngineAndTires/GenericConstrains/EqualityScale.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericScale
+{
+   public class EqualityScale<T>
+        where T : class
+    {
+        public EqualityScale(T Left, T Right)
+        {
+            this.Left = Left;
+            this.Right = Right;
+        }
+
+        private T Left { get; set; }
+
+        private T Right { get; set; }
+
+        public bool AreEqual()
+        {
+            return Left.Equals(Right); 
+        }
+    }
+}
---
+++ ClassesEx/CarEngineAndTires/GenericConstrains/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GenericScale
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+          // var smth = new EqualityScale<int>(15, 15);
+          //
+          // Console.WriteLine(smth.AreEqual());
+
+            string first = "first";
+            string second = "first";
+            var refTypeData = new EqualityScale<string>(first, second);
+
+            Console.WriteLine(refTypeData.AreEqual());
+        }
+    }
+}
