91be30721a969ca75cdb1d74304a0111e785c8e6
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-10-16 14:31:09 +0300

    Exercise with the override method in a generic class

---
+++ ClassesEx/CarEngineAndTires/OverRide Generic types/Box.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericBoxOfString
+{
+   public class Box<T>
+    {
+        public Box()
+        {
+            Values = new List<T>();
+        }
+        private List<T> Values { get; set; }
+
+        public void Add(T item)
+        {
+            Values.Add(item);
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            foreach (var item in Values)
+            {
+                sb.AppendLine($"System.String: {item}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+
+}
---
+++ ClassesEx/CarEngineAndTires/OverRide Generic types/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GenericBoxOfString
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int dataLength = int.Parse(Console.ReadLine());
+
+            var newClass = new Box<string>();
+
+            for (int i = 0; i < dataLength; i++)
+            {
+                newClass.Add(Console.ReadLine());
+            }
+
+            Console.WriteLine(newClass);
+
+        }
+    }
+}
