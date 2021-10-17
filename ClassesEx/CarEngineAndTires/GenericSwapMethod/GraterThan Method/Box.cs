7185952643c0d4f45cc842201b36d502b8ff49f6
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-10-17 12:28:06 +0300

    Other method including constrains

---
+++ ClassesEx/CarEngineAndTires/GenericSwapMethod/GraterThan Method/Box.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericBoxOfString
+{
+    public class Box<T>
+        where T : IComparable
+    {
+        public Box()
+        {
+            Values = new List<T>();
+        }
+        private List<T> Values { get; set; }
+
+        public void Add(T item)
+        {
+            Values.Add(item);
+        }
+
+        public void Swap(int firstIndex, int secondIndex)
+        {
+            var firstIndexSaved = Values[firstIndex];
+            Values[firstIndex] = Values[secondIndex];
+            Values[secondIndex] = firstIndexSaved;
+            
+        }
+        public int GraterThan(T element)
+        {
+            int counter = 0;
+            foreach (var item in Values)
+            {
+                if (item.CompareTo(element) > 0)
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            foreach (var item in Values)
+            {
+                sb.AppendLine($"System.Int32: {item}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+
+}
---
+++ ClassesEx/CarEngineAndTires/GenericSwapMethod/GraterThan Method/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Linq;
+
+namespace GenericBoxOfString
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int dataLength = int.Parse(Console.ReadLine());
+
+            var newClass = new Box<string>();
+
+            for (int i = 0; i < dataLength; i++)
+            {
+                newClass.Add(Console.ReadLine());
+            }
+
+            var element = Console.ReadLine();
+            Console.WriteLine(newClass.GraterThan(element));
+
+        }
+    }
+}
