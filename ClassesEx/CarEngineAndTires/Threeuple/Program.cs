fef27c5aaf930f1fa7138287334f7605ecad4df1
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-10-17 13:08:09 +0300

    Basic Class With Three Generic Items

---
+++ ClassesEx/CarEngineAndTires/Threeuple/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace TupleCLass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] firstLine = Console.ReadLine().Split(' ');
+            string name = firstLine[0] + " " + firstLine[1];
+
+            var T1 = new Threeuple<string, string, string>(name, firstLine[2], firstLine[3]);
+            T1.ConsoleWritline();
+
+
+            string[] secondLine = Console.ReadLine().Split(' ');
+            bool drunk = true;
+
+            if (secondLine[1] == "drunk")
+            {
+                drunk = true;
+            }
+            else
+            {
+                drunk = false;
+            }
+
+            var T2 = new Threeuple<string, int, bool>(secondLine[0], int.Parse(secondLine[1]), drunk);
+            T2.ConsoleWritline();
+
+            string[] thirdLine = Console.ReadLine().Split(' ');
+            var T3 = new Threeuple<string, double, string>(thirdLine[0], double.Parse(thirdLine[1]), thirdLine[2]);
+            T3.ConsoleWritline();
+
+        }
+    }
+}
---
+++ ClassesEx/CarEngineAndTires/Threeuple/Threeuple.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TupleCLass
+{
+    class Threeuple<T1, T2, T3>
+    {
+
+        public Threeuple(T1 firstI, T2 secondI, T3 third)
+        {
+            FirstItem = firstI;
+            SecondItem = secondI;
+            ThirdItem = third;
+        }
+        public T1 FirstItem { get; set; }
+
+        public T2 SecondItem { get; set; }
+
+        public T3 ThirdItem { get; set; }
+
+        public void ConsoleWritline()
+        {
+
+            Console.WriteLine(FirstItem + " -> " + SecondItem + " -> " + ThirdItem );
+        }
+    }
+}
