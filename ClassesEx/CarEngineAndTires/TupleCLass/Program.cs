df7d6f8f837b7cc8a882fdb4f8d721b03dbf4a5b
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-10-17 12:47:50 +0300

    Custom Tuple Class

---
+++ ClassesEx/CarEngineAndTires/TupleCLass/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TupleCLass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string[] firstLine = Console.ReadLine().Split(' ');
+            var name = new Tuple<string, string>(firstLine[0] + " "+ firstLine[1], firstLine[2]);
+
+            Console.WriteLine(name.FirstItem + " -> " + name.SecondItem);
+
+            string[] secondLine = Console.ReadLine().Split(' ');
+            var secondTuple = new Tuple<string, int>(secondLine[0], int.Parse(secondLine[1]));
+
+            Console.WriteLine(secondTuple.FirstItem + " -> " + secondTuple.SecondItem);
+
+            string[] thirdLine = Console.ReadLine().Split(' ');
+            int firstItem = int.Parse(thirdLine[0]);
+            double secondItem = double.Parse(thirdLine[1]);
+            var thirdTuple = new Tuple<int, double>(firstItem, secondItem);
+            Console.WriteLine(thirdTuple.FirstItem + " -> " + thirdTuple.SecondItem);
+
+
+        }
+    }
+}
---
+++ ClassesEx/CarEngineAndTires/TupleCLass/Tuple.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TupleCLass
+{
+    class Tuple<T1 , T2>
+    {
+
+        public Tuple(T1 firstI, T2 secondI)
+        {
+            FirstItem = firstI;
+            SecondItem = secondI;
+        }
+        public T1 FirstItem { get; set; }
+
+        public T2 SecondItem { get; set; }
+    }
+}
