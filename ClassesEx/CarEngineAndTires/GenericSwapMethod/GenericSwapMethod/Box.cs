f633c16cdd1150b5d5851330013740528fde51bf
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-10-17 11:48:18 +0300

    Same template, but with Integers

---
+++ ClassesEx/CarEngineAndTires/GenericSwapMethod/GenericSwapMethod/Box.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericBoxOfString
+{
+    public class Box<T>
+    {
+        public Box()
+        {
+            Values = new List<T>();
+        }
+        private List<T> Values { get; set; }
+
+        public void Add(T item)
+        {
+            Values.Add(item);
+        }
+
+        public void Swap(int firstIndex, int secondIndex)
+        {
+            var firstIndexSaved = Values[firstIndex];
+            Values[firstIndex] = Values[secondIndex];
+            Values[secondIndex] = firstIndexSaved;
+            
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            foreach (var item in Values)
+            {
+                sb.AppendLine($"System.Int32: {item}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+
+}
---
+++ ClassesEx/CarEngineAndTires/GenericSwapMethod/GenericSwapMethod/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Linq;
+
+namespace GenericBoxOfString
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int dataLength = int.Parse(Console.ReadLine());
+
+            var newClass = new Box<int>();
+
+            for (int i = 0; i < dataLength; i++)
+            {
+                newClass.Add(int.Parse(Console.ReadLine()));
+            }
+
+            int[] indexes = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            newClass.Swap(indexes[0], indexes[1]);
+
+
+            Console.WriteLine(newClass);
+
+        }
+    }
+}
