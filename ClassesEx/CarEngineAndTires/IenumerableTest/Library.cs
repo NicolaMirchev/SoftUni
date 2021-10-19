f942ee89586dc11c46d808a0fc2b18136d646e00
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-10-19 12:05:03 +0300

    Class Library

---
+++ ClassesEx/CarEngineAndTires/IenumerableTest/Library.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    class Library : IEnumerable<Book>
+    {
+
+        public Library()
+        {
+            books = new List<Book>();
+        }
+        public Library(params Book[] books)
+        {
+            this.books = new List<Book>(books);
+        }
+        private List<Book> books { get; set; }
+
+        public void AddBook(string name, List<string> authors, int year)
+        {
+            books.Add(new Book(name, authors, year));            
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ ClassesEx/CarEngineAndTires/IenumerableTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+
+namespace IteratorsAndComparators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var library = new Library();
+            library.AddBook()
+    }
+}
