fc29bdeac786c7620623ba10aa453bcc28912ac1
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-10-27 12:46:57 +0300

    Inheriting the list class

---
+++ CsOOP/RandomList/RandomList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomRandomList
+{
+    class RandomList : List<string>
+    {
+
+        public string RandomString()
+        {
+            var r = new Random();
+
+            int index = r.Next(0, Count);
+
+            return this[index];
+        }
+    }
+}
