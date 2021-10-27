d06c708da8195ac1f16b9f7a6f34743ff11dbd36
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-10-27 12:06:11 +0300

    Custom caesars shift

---
+++ CryptingMessage/CryptingMessage/Encrypting.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptingMessage
+{
+    class Encrypting
+    {
+
+
+       public static string Encrypt(string text)
+        {
+            char[] answer = new char[text.Length];
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                answer[i] = (char) (97 + ((text[i] + 3 - 97) % 26));                
+            }
+
+            string stringAnswer = "";
+            foreach (var item in answer)
+            {
+                stringAnswer += item;
+            }
+            return stringAnswer;
+        }
+
+        public static string Decrypt(string text)
+        {
+            char[] charArr = new char[text.Length];
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == 'a' || text[i] == 'b' || text[i] == 'c')
+                {
+                    charArr[i] = (char) (text[i] + 23);
+                }
+                else if (text[i] == 'W')
+                {
+                    charArr[i] = ' ';
+                }
+                else
+                {
+                charArr[i] = (char) (97 + ((text[i] - 3 - 97) % 26));
+                }
+            }
+            string answer = "";
+            foreach (var item in charArr)
+            {
+                answer += item;
+            }
+            return answer;
+        }
+    }
+}
---
+++ CryptingMessage/CryptingMessage/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CryptingMessage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string input = Console.ReadLine();
+
+            string result = Encrypting.Encrypt(input);
+            Console.WriteLine(result);
+
+            Console.WriteLine(Encrypting.Decrypt(result));
+        }
+    }
+}
