96bbe5349a74805548ce70b124b4cbdee7b483ff
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-10-27 12:49:36 +0300

    Inheriting from the stack class

---
+++ CsOOP/Inheritance/CustomStack/StackOfStrings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    class StackOfStrings : Stack<string>
+    {
+
+        public bool IsEmpty()
+        {
+            return this.Count == 0;
+        }
+
+        public void AddRange(IEnumerable<string> range)
+        {
+            foreach (var item in range)
+            {
+                this.Push(item);
+            }
+        }
+    }
+}
