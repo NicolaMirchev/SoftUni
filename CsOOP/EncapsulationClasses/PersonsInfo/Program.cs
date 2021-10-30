69097c105efa029563d89fd10a85b8a7f34ce9e8
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-10-30 17:41:37 +0300

    Private set

---
+++ CsOOP/EncapsulationClasses/PersonsInfo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonsInfo
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            var people = new List<Person>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                string[] personData = Console.ReadLine().Split();
+                var newPerson = new Person(personData[0], personData[1], int.Parse(personData[2]));
+
+                people.Add(newPerson);
+            }
+            people = people.OrderBy(p => p.FirstName).ThenBy(p => p.Age).ToList();
+            foreach (var person in people)
+            {
+                Console.WriteLine(person);
+            }
+        }
+    }
+}
