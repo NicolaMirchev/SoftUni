58c7688d5015fa238fb08688eeafe18c57ea9f86
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-10-30 18:32:53 +0300

    New One

--- CsOOP/EncapsulationClasses/Program.cs
+++ CsOOP/EncapsulationClasses/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace EncapsulationClasses
+namespace PersonsInfo
 {
-    class Program
+   public class StartUp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int lines = int.Parse(Console.ReadLine());
+            var people = new List<Person>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                string[] personData = Console.ReadLine().Split();
+                var newPerson = new Person(personData[0], personData[1],
+                                int.Parse(personData[2]), decimal.Parse(personData[3]));
+
+                people.Add(newPerson);
+            }
+
+            int percentage = int.Parse(Console.ReadLine());
+            foreach (var person in people)
+            {
+                person.IncreaseSalary(percentage);
+            }
+            foreach (var person in people)
+            {
+                Console.WriteLine(person);
+            }
         }
     }
 }
