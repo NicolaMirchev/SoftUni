7b4b2ff66deed7fd10717f1b3949d423536cc9bc
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-10-30 10:27:58 +0300

    Creating folder

---
+++ CsOOP/EncapsulationClasses/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EncapsulationClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
