c3a9e9bcb490a84625f2fa7e831fc00c9c91b3ee
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-11-02 22:11:26 +0200

    Implemeting Interface

---
+++ Drawing/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Drawing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var cirle = new Circle(6);
+            IDrawoble rectangle = new Rectangle(10, 5);
+
+            rectangle.Draw();         
+        }
+    }
+}
---
+++ Drawing/Rectangle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Drawing
+{
+    class Rectangle : IDrawoble
+    {
+
+        public Rectangle(int width, int high)
+        {
+            Width = width;
+            High = high;
+        }
+        public int Width { get; private set; }
+        public int High { get; private set; }
+        public void Draw()
+        {
+            for (int i = 0; i < Width; i++)
+            {
+                Console.Write('*');
+            }
+            Console.WriteLine();
+            for (int i = 1; i < High - 1; i++)
+            {
+                Console.Write('*');
+                Console.SetCursorPosition(Width - 1, i);
+                Console.WriteLine('*');
+            }
+            for (int i = 0; i < Width; i++)
+            {
+            Console.Write('*');
+            }
+        }
+    }
+}
