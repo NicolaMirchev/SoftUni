6c0a56a8b715d0dbdcf7974b26a2bbec0e0253bc
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-11-05 14:50:25 +0200

    Override methods

---
+++ CsOOP/Animals/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Animals
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var cat = new Cat("Ciska", "Wiskas");
+
+            Console.WriteLine(cat.ExplainSelf());
+        }
+    }
+}
