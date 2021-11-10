d76e80609398acb296652357c10512fe7b58d0bf
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-11-10 23:15:38 +0200

    Try cach

---
+++ CsOOP/NumberValidator/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace NumberValidator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = 5;
+            int end = 20;
+            try
+            {
+                ReadNumber(int.Parse("neshtosi"), start);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Input should be valid integer!");
+            }
+            finally
+            {
+                Console.Beep(500, 1);
+                Console.Beep(400, 5);
+                Console.Beep(300, 1);
+                Console.Beep(200, 5);
+                Console.Beep(100, 10);
+            }
+        }
+
+             public static void ReadNumber(int start, int end)
+            {
+                int i = start;
+                try
+                {
+                    while (i != end)
+                    {
+                        Console.WriteLine(i);
+                        i++;
+                    }
+
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            
+        }
+    }
+}
