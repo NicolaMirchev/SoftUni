9e79897118b35bda835c5857b797ceaffc8e107e
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-11-11 09:53:35 +0200

    Experiment with exeptions

---
+++ CsOOP/ExeptionHandling/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ExeptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] weekDays = new string[]
+            {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Satarday",
+                "Sunday"
+            };
+
+            for (int i = 0; i <= weekDays.Length ; i++)
+            {
+                try
+                {
+                    Console.WriteLine(weekDays[i]);
+                }
+                catch (IndexOutOfRangeException exeption)
+                {
+                    
+                    Console.WriteLine("Loop is bigger than the array!"
+                        + $"\n {exeption.Source}");
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Error");
+                }
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
