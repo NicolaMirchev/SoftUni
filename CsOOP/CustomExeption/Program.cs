2bf1135c7566d5e219679dadc1a0c7290109a41e
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-11-11 11:13:17 +0200

    Throwing custom exeption

---
+++ CsOOP/CustomExeption/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace NumberValidator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] data = Console.ReadLine().Split(' ');
+            string firstName = data[0];
+            string lastName = data[1];
+            int age = 0;
+            bool inValid = true;
+            while (inValid)
+            {
+                try
+                {
+                    age = int.Parse(Console.ReadLine());
+                    inValid = false;
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message + "Reenter the age");
+                }
+
+
+
+            }
+
+
+            Person person = null;
+            bool validPersonIsCreated = false;
+            while (!validPersonIsCreated)
+            {
+
+                try
+                {
+                    person = new Person(firstName, lastName, age);
+                    validPersonIsCreated = true;
+                }
+                catch (IndexOutOfRangeException exeption)
+                {
+                    Console.WriteLine(exeption.Message );
+                    Console.WriteLine("Reenter the age.");
+                    age = int.Parse(Console.ReadLine());
+                }
+                catch (ArgumentNullException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("Reenger the name");
+                    if (string.IsNullOrEmpty(firstName))
+                    {
+                    firstName = Console.ReadLine();
+                    }
+                    else
+                    {
+                        lastName = Console.ReadLine();
+                    }
+                }
+            }
+
+            Console.WriteLine(person);
+            try
+            {
+            Student student = new Student("Vank0", "PetkoV", 72);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                
+            }
+        }
+    }
+}
---
+++ CsOOP/CustomExeption/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NumberValidator
+{
+    class Student : Person
+    {
+        private string name;
+
+        public Student(string name, string lastName, int age) : base(name, lastName, age)
+        {
+            this.Name = name;
+        }
+
+        public new string Name { get => name;
+            set
+            {
+                for (int i = 0; i < value.Length; i++)
+                {
+                    if (char.IsDigit(value[i]) || char.IsSymbol(value[i]))
+                    {
+                        throw new InvalidPersonNameExeption();
+                    }
+                }
+                Name = value; 
+            } 
+        }
+    }
+}
