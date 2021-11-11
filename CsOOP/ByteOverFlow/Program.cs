457cb7e653808fbe154ce27614466ffdea61c7c3
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-11-11 10:03:24 +0200

    Exersice

---
+++ CsOOP/ByteOverFlow/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ExeptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber, secondNumber;
+            byte result;
+
+            firstNumber = 30;
+            secondNumber = 60;
+            try
+            {
+                result = Convert.ToByte(firstNumber * secondNumber);
+                Console.WriteLine(result);
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Type byte can be fewer than " + byte.MaxValue);
+            }
+
+        }
+    }
+}
