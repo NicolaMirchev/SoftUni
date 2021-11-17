4708788a30b6b6ff89c894b1af76ce3d571c7c82
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-11-17 16:30:20 +0200

    Reflection

---
+++ CsOOP/Reflection/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reflection
+{
+    abstract class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public void Sleep()
+        {
+            Console.WriteLine("Sleeping..");
+        }
+    }
+}
