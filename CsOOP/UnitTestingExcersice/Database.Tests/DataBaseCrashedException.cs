c955cef887478d9479897e709a865c827e1f0803
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-11-23 22:12:05 +0200

    Assert to catch custom created exception

---
+++ CsOOP/UnitTestingExcersice/Database.Tests/DataBaseCrashedException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Database
+{
+   public class DataBaseCrashedException : Exception
+    {
+        public DataBaseCrashedException(string message) : base(message)
+        { }
+
+    }
+}
