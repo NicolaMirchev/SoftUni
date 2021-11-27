3006b1508570a9eb944983acb8afa35ec98a7827
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-11-27 17:33:50 +0200

    Tests about the funcionallity of a custom class

---
+++ CsOOP/INStock/INStock.Tests/ProductStockTests.cs
@@ -0,0 +1,24 @@
+﻿namespace INStock.Tests
+{
+    using Moq;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ProductStockTests
+    {
+
+        [Test]
+        public void ChekTheAddMethodInTheStock()
+        {
+            Product productCoke = new Coke("1111", 10, 20);
+
+            Stock stock = new Stock();
+
+            stock.Add(productCoke);
+
+            Assert.AreEqual(1, stock.Count);
+            Assert.IsTrue(stock.Contains(productCoke));
+        }
+
+    }
+}
