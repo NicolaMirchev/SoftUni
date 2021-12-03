5c2203d5105f3f5f7a736da5028be955ffb9b6a1
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-12-03 14:04:26 +0200

    Organizing Classes And Their Methods

---
+++ CsOOP/TIcTacToe/TIcTacToe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TIcTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
