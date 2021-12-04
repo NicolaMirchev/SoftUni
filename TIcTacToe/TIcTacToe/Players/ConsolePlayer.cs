b9cc5aac0bdfa8ecba02cb33ece3082f2506598f
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-12-04 10:52:23 +0200

    Implemented game engine, random and console player

---
+++ TIcTacToe/TIcTacToe/Players/ConsolePlayer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TIcTacToe.Players
+{
+    class ConsolePlayer : IPlayer
+    {
+
+        public ConsolePlayer(Symbol symbol)
+        {
+            CurrentPlayerSymbol = symbol;
+        }
+        public Symbol CurrentPlayerSymbol { get ; set ; }
+
+        public Index IndexToPlace(IBoard board)
+        {
+            Console.WriteLine(board);
+            bool validInput = false;
+            Index result = new Index();
+
+            while (!validInput)
+            {
+            try
+            {
+            int[] index = Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+                    
+                    validInput = true;
+                    result.Row = index[0];
+                    result.Col = index[1];
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid input.");
+            }
+
+            }
+
+            return result;
+
+        }
+    }
+}
---
+++ TIcTacToe/TIcTacToe/Players/RandomPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TIcTacToe.Players
+{
+    class RandomPlayer : IPlayer
+    {
+        public RandomPlayer(Symbol symbol)
+        {
+            CurrentPlayerSymbol = symbol;
+        }
+        public Symbol CurrentPlayerSymbol { get ; set ; }
+
+        public Index IndexToPlace(IBoard board)
+        {
+            var emptyPositions = board.GetEmptyPositions();
+
+            Random randomPositionsPicker = new Random();
+
+            int indexToPLay = randomPositionsPicker.Next(0, emptyPositions.Count - 1);
+            return emptyPositions[indexToPLay];
+        }
+    }
+}
