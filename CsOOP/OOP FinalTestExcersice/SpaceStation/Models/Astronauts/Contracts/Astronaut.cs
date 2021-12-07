62c88b7f20f03061fdd889fa006da025b5a4aa97
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-12-07 21:30:24 +0200

    OOP Exercise

---
+++ CsOOP/OOP FinalTestExcersice/SpaceStation/Models/Astronauts/Contracts/Astronaut.cs
@@ -0,0 +1,27 @@
+﻿using SpaceStation.Models.Bags.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceStation.Models.Astronauts.Contracts
+{
+    public abstract class Astronaut : IAstronaut
+    {
+        public Astronaut(string name, double oxygen)
+        {
+
+        }
+        public string Name => throw new NotImplementedException();
+
+        public double Oxygen => throw new NotImplementedException();
+
+        public bool CanBreath => throw new NotImplementedException();
+
+        public IBag Bag => throw new NotImplementedException();
+
+        public virtual void Breath()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CsOOP/OOP FinalTestExcersice/SpaceStation/Repositories/Contracts/PlanetRepository.cs
@@ -0,0 +1,27 @@
+﻿using SpaceStation.Models.Planets.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceStation.Repositories.Contracts
+{
+   public class PlanetRepository : IRepository<Planet>
+    {
+        public IReadOnlyCollection<Planet> Models => throw new NotImplementedException();
+
+        public void Add(Planet model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Planet FindByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(Planet model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
