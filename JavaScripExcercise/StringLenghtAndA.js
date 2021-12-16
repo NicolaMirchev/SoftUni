7b3a28b7c5185f0982c8039af3971edfaff4ca8c
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-12-16 11:04:21 +0200

    Add files via upload

---
+++ JavaScripExcercise/StringLenghtAndA.js
@@ -0,0 +1,14 @@
+
+
+function stringInfo(a, b, c)
+{
+
+    let allStringsCount = a.length + b.length + c.length;
+    console.log(allStringsCount);
+    console.log(Math.floor(allStringsCount/3))
+    
+
+}
+stringInfo('chocolate' , 'ice cream' , 'cake');
+
+
