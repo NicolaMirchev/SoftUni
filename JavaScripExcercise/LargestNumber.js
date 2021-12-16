c65fd6c0bc03d65a67d0ee805fa3b2613a6f0a62
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2021-12-16 11:15:04 +0200

    Build-in Math func

---
+++ JavaScripExcercise/LargestNumber.js
@@ -0,0 +1,16 @@
+
+
+function findLargestNum(a, b, c)
+{
+   
+      
+    console.log(`The largest number is ${Math.max(a,b,c)}.`);
+
+}
+
+findLargestNum(4, 5, 7);
+findLargestNum(4, 8, 7);
+findLargestNum(4, 1, 7);
+findLargestNum(66, 5, 7);
+
+
