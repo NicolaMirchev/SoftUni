2378e71e2a4e4c4da27230ec350c7817915abae2
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2022-06-02 17:43:14 +0300

    Explicit Loading Entittie References

--- EntityFrameworkExercise/BookShop/StartUp.cs
+++ EntityFrameworkExercise/BookShop/StartUp.cs
@@ -3,26 +3,45 @@
     using BookShop.Models.Enums;
     using Data;
     using Initializer;
+    using Microsoft.EntityFrameworkCore;
     using System;
     using System.Linq;
     using System.Text;
+    using System.Threading.Tasks;
 
     public class StartUp
     {
         public static void Main()
         {
             using var db = new BookShopContext();
-             DbInitializer.ResetDatabase(db);
+            // DbInitializer.ResetDatabase(db);
 
-            string command = Console.ReadLine();
-
-            System.Console.WriteLine(GetBooksByAgeRestrictions(db, command));
+            Console.WriteLine(GetGoldenBooks(db));
 
 
         }
 
 
+        public static  string GetGoldenBooks(BookShopContext context)
+        {
 
+            var filteredBooks = context.Books.Where(b => b.EditionType == EditionType.Gold)
+                                             .Where(b => b.Copies < 5000)
+                                             .OrderBy(b => b)
+                                             .ToArray();
+
+            var sb = new StringBuilder();
+
+
+
+            foreach (var item in filteredBooks)
+            {
+                context.Entry(item).Reference(b => b.Author).Load();
+                Console.WriteLine(item.Author.FirstName);
+            }
+
+            return  sb.ToString();
+        }
         public static string GetBooksByAgeRestrictions(BookShopContext context, string command)
         {
             AgeRestriction ageRestriction= new AgeRestriction();
