2aa4ffba7ffe5f2edcee78ad393a8c8afd7efeba
NikolaM <78795830+NicolaMirchev@users.noreply.github.com>
2022-06-07 11:55:21 +0300

    Using db Softuni to extract employees and serialize the object to write it to file

--- EntityFrameworkExercise/Program.cs
+++ EntityFrameworkExercise/Program.cs
@@ -3,15 +3,54 @@
 using EntityFrameworkExercise;
 using EntityFrameworkExercise.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+using (var db = new SoftUniContext())
+{
+    var employeesInSales = db.Employees
+        .Where(s => s.Department.Name == "Sales" && s.Salary > 15000)
+        .Select(e => new
+        { 
+             Name = e.FirstName + " " + e.LastName,
+             Department  = e.Department,
+             Salary = e.Salary,
+             JobTitle = e.JobTitle,                
+        })
+        .ToArray();
+
+    List<string> jsonStrings = new List<string>();
+    foreach (var employee in employeesInSales)
+    {
+        JsonSerializerOptions options = new JsonSerializerOptions()
+        {
+            ReferenceHandler = ReferenceHandler.IgnoreCycles
+        };
+
+        string serializedObj = JsonSerializer.Serialize(employee,options);
+        jsonStrings.Add(serializedObj);
+        Console.WriteLine(serializedObj);
+    }
+
+        File.AppendAllLines("employees.json", jsonStrings);
+
+}
 
 
-var db = new SoftUniContext();
-
-
-Console.WriteLine(StartUp.GetEmployeesFullInformation(db));
 
 
 
+//Department dep = new Department();
+//dep.Name = "12345678910123456789101234567891012345678910123456789101234567891012345678910";
+
+//dep.Manager = db.Employees.FindAsync(1).Result.Manager;
+
+
+//await db.Departments.AddAsync(dep);
+
+//await db.SaveChangesAsync();
+
+//Console.WriteLine(db.Employees.FindAsync(2).Result.Salary);
 
 
 
